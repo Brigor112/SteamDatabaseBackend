33c58f77e37bb5f368fd4edb81ef740ad9336d70
Brigor112 <46456996+Brigor112@users.noreply.github.com>
2019-01-07 18:40:12 +0000

    APP.cstest
    testing my SQL

--- Database/App.cs
+++ Database/App.cs
@@ -3,14 +3,5 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-namespace SteamDatabaseBackend
-{
-    struct App
-    {
-        public uint AppID { get; set; }
-        public uint AppType { get; set; } // Not EAppType
-        public string AppTypeString { get; set; }
-        public string Name { get; set; }
-        public string LastKnownName { get; set; }
-    }
-}
+Select * from SteamDatabaseBackend
+Limit 10
